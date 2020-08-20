f0b14a9d642dffa969879054c94d648187f1b465
Carter Allen <carter.allen12@gmail.com>
2020-08-20 09:55:15 -0400

    sampling functions

---
+++ R/sample_mlsbm.R
@@ -0,0 +1,28 @@
+#' R/Rcpp function for sampling from a multilevel stochastic block model
+#'
+#' This function allows you to sample a multilevel stochastic block model.
+#' @param z An n x 1 vector of community labels for each node
+#' @param P A K x K symmetric matrix of community connectivity probabilities
+#' @param L The number of levels to sample
+#' @keywords SBM
+#' @export
+#' @examples
+#' n = 100
+#' K = 3
+#' L = 2
+#' pi = rep(1/K,K)
+#' z = sample(1:K, size = n, replace = TRUE, prob = pi)
+#' p_in = 0.50
+#' p_out = 0.05
+#' P = matrix(p_out, nrow = K, ncol = K)
+#' diag(P) = p_in
+#' AL = sample_mlsbm(z,P,L)
+sample_mlsbm <- function(z,P,L)
+{
+  AL = vector("list",L)
+  for(l in 1:L)
+  {
+    AL[[l]] = sample_sbm(z,P)
+  }
+  return(AL)
+}
---
+++ R/sample_sbm.R
@@ -0,0 +1,22 @@
+#' R/Rcpp function for sampling from a single level stochastic block model
+#'
+#' This function allows you to sample a single level stochastic block model.
+#' @param z An n x 1 vector of community labels for each node
+#' @param P A K x K symmetric matrix of community connectivity probabilities
+#' @keywords SBM
+#' @export
+#' @examples
+#' n = 100
+#' K = 3
+#' pi = rep(1/K,K)
+#' z = sample(1:K, size = n, replace = TRUE, prob = pi)
+#' p_in = 0.50
+#' p_out = 0.05
+#' P = matrix(p_out, nrow = K, ncol = K)
+#' diag(P) = p_in
+#' A = sample_sbm(z,P)
+sample_sbm <- function(z,P)
+{
+  A = sample_SBM_fast(z,P)
+  return(A)
+}
